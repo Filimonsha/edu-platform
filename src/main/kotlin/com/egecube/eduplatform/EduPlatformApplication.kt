4480ecd20853bebf45cdf4cea6cc66fc0922b00c
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-04-29 22:51:27 +0300

    init commit

---
+++ src/main/kotlin/com/egecube/eduplatform/EduPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.egecube.eduplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduPlatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<EduPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/egecube/eduplatform/EduPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.egecube.eduplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EduPlatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
