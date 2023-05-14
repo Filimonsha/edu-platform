0fabeac7c8911bccdeded3b96f58424a1596c19c
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2023-05-14 21:08:28 +0300

    feat: start schedule service

---
+++ src/main/kotlin/com/egecube/eduplatform/schedules/controllers/ScheduleController.kt
@@ -0,0 +1,7 @@
+package com.egecube.eduplatform.schedules.controllers
+
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduleController {
+}
---
+++ src/main/kotlin/com/egecube/eduplatform/schedules/repositories/ScheduleRepository.kt
@@ -0,0 +1,8 @@
+package com.egecube.eduplatform.schedules.repositories
+
+import org.springframework.stereotype.Service
+
+@Service
+class ScheduleRepository {
+
+}
