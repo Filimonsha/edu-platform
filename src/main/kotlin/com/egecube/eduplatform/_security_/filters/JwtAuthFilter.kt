02346d16d9223b6d0b1b211fb53f012eeb890ac4
filberol <mkh.vadim@gmail.com>
2023-07-18 15:44:31 +0300

    fix: enable security

--- src/main/kotlin/com/egecube/eduplatform/_security_/filters/JwtAuthFilter.kt
+++ src/main/kotlin/com/egecube/eduplatform/_security_/filters/JwtAuthFilter.kt
@@ -2,7 +2,9 @@ package com.egecube.eduplatform._security_.filters
 
 //import com.egecube.eduplatform._security_.access_rights_utils.AccessRightsService
 //import com.egecube.eduplatform._security_.jwt_utils.JwtService
+import com.egecube.eduplatform._security_.access_rights_utils.AccessRightsService
 import com.egecube.eduplatform._security_.accounts.domain.UserRole
+import com.egecube.eduplatform._security_.jwt_utils.JwtService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -15,8 +17,8 @@ import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class JwtAuthFilter(
-//    private val accessRightsService: AccessRightsService,
-//    private val jwtService: JwtService
+    private val accessRightsService: AccessRightsService,
+    private val jwtService: JwtService
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -25,26 +27,26 @@ class JwtAuthFilter(
         filterChain: FilterChain
     ) {
         // NO SECURITY FOR DEV MAIN BRANCH
-//        try {
-//            val jwt = accessRightsService.extractJwtIfPresentInRequest(request)
-//            val userMail = jwtService.extractUserMail(jwt!!)
+        try {
+            val jwt = accessRightsService.extractJwtIfPresentInRequest(request)
+            val userMail = jwtService.extractUserMail(jwt!!)
             if (SecurityContextHolder.getContext().authentication == null) {
                 // Update auth context for filters
-//                if (jwtService.isTokenValid(jwt, userMail!!)) {
+                if (jwtService.isTokenValid(jwt, userMail!!)) {
                     val auth = UsernamePasswordAuthenticationToken(
-//                        userMail,
-                        "def_user", // username
+                        userMail,
+//                        "def_user", // username
                         1, // get id from db if not authenticated
                         arrayListOf(SimpleGrantedAuthority(UserRole.ADMIN.name))
                     )
                     auth.details = WebAuthenticationDetailsSource().buildDetails(request)
                     SecurityContextHolder.getContext().authentication = auth
                 }
-//            }
+            }
             filterChain.doFilter(request, response)
-//        } catch (e: NullPointerException) {
-//            filterChain.doFilter(request, response)
-//            return
-//        }
+        } catch (e: NullPointerException) {
+            filterChain.doFilter(request, response)
+            return
+        }
     }
 }
